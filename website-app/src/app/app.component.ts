a6c0aa7932a14d9c74461027469bd85ef7aaacdb
Carlos Zatarain <Niarataz01@gmail.com>
2024-08-03 21:21:14 -0500

    Add changing hue glow for header/footer

--- website-app/src/app/app.component.ts
+++ website-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -11,6 +11,33 @@ import { SettingsComponent } from './settings/settings.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'website-app';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.injectRainbowAnimation();
+  }
+
+  injectRainbowAnimation() {
+    const styleSheet = this.renderer.createElement('style');
+    styleSheet.type = 'text/css';
+    const hueSteps = 20; // Increase for finer transitions
+    let keyframes = `@keyframes rainbow {`;
+
+    for (let i = 0; i <= 100; i += 100 / hueSteps) {
+      const hue = (i / 100) * 360;
+      keyframes += `
+        ${i}% {
+          box-shadow: 0 0 10px 2px hsla(${hue}, 100%, 50%, 0.3);
+        }
+      `;
+    }
+
+    keyframes += `}`;
+
+    styleSheet.innerHTML = keyframes;
+    this.renderer.appendChild(document.head, styleSheet);
+  }
 }
