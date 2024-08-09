13a1adf5f03ae7064141131d545de58a8efab770
Carlos Zatarain <Niarataz01@gmail.com>
2024-08-08 21:27:30 -0500

    Finish first blog post

--- website-app/src/app/blog/blog-list/blog-list.component.ts
+++ website-app/src/app/blog/blog-list/blog-list.component.ts
@@ -5,6 +5,7 @@ import { PostService } from '../../post.service';
 interface Post {
   id: string;
   title: string;
+  category: string;
 }
 
 interface Category {
@@ -19,22 +20,35 @@ interface Category {
   styleUrls: ['./blog-list.component.css']
 })
 export class BlogListComponent implements OnInit {
-  categories: Category[] = [
-    { 
-      name: 'Programming', 
-      expanded: false, 
-      posts: [
-        { 
-          id: 'post1', 
-          title: 'Making This Website - 8/8/2024' 
-        }
-      ] 
-    }
-  ];
+  categories: Category[] = [];
 
   constructor(private router: Router, private postService: PostService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeCategories();
+  }
+
+  initializeCategories(): void {
+    const posts = this.postService.getAllPosts();
+
+    // Group posts by category
+    const categoryMap: { [key: string]: Post[] } = {};
+
+    posts.forEach(post => {
+      const categoryName = post.category;
+      if (!categoryMap[categoryName]) {
+        categoryMap[categoryName] = [];
+      }
+      categoryMap[categoryName].push(post);
+    });
+
+    // Convert the dictionary to an array of categories
+    this.categories = Object.keys(categoryMap).map(categoryName => ({
+      name: categoryName,
+      expanded: false,
+      posts: categoryMap[categoryName]
+    }));
+  }
 
   toggleCategory(category: Category) {
     category.expanded = !category.expanded;
--- website-app/src/app/post.service.ts
+++ website-app/src/app/post.service.ts
@@ -4,6 +4,7 @@ interface Post {
   id: string;
   title: string;
   content: string;
+  category: string;
 }
 
 @Injectable({
@@ -11,10 +12,14 @@ interface Post {
 })
 export class PostService {
   private posts: Post[] = [
-    { id: 'post1', title: 'Making This Website', content: 'This is the content of post1.' }
+    { id: 'post1', title: 'Making This Website - 8/8/2024', content: 'This is the content of post1.', category: "Programming"}
   ];
 
   getPostById(id: string): Post | undefined {
     return this.posts.find(post => post.id === id);
+  }
+
+  getAllPosts(): Post[] {
+    return this.posts;
   }
 }
