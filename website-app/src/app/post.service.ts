4f73b920eb8321ea83785987e122c82f4cc558e8
Carlos Zatarain <Niarataz01@gmail.com>
2024-10-03 14:54:36 -0500

    Add third blog post

--- website-app/src/app/post.service.ts
+++ website-app/src/app/post.service.ts
@@ -13,7 +13,8 @@ interface Post {
 export class PostService {
   private posts: Post[] = [
     { id: 'post1', title: 'Making This Website - 8/8/2024', content: '', category: "Programming"},
-    { id: 'post2', title: 'XP Ransomware Analysis - 8/9/2024', content: '', category: "Programming"}
+    { id: 'post2', title: 'XP Ransomware Analysis - 8/9/2024', content: '', category: "Programming"},
+    { id: 'post3', title: 'How I landed an Offer with Google - 10/3/2024', content: '', category: "Personal Life"}
   ];
 
   getPostById(id: string): Post | undefined {
